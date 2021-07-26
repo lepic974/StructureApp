70096af123e80a326748906eeb18bf9f1bf0f7e6
Johan <johan.lepinay@gmail.com>
2021-07-26 11:21:05 +0400

    First commit

--- StructureApp/Controllers/ContentView.swift
+++ StructureApp/Controllers/ContentView.swift
@@ -11,6 +11,11 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .foregroundColor(.black)
+            .toolbar {
+                Image(systemName: "person")
+            }
+
     }
 }
 
---
+++ StructureApp/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  StructureApp
+//
+//  Created by Joh_Macbook on 26/07/2021.
+//
+
+import UIKit
+import SwiftUI
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
